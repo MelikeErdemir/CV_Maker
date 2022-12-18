7bfae97f8248a6a0a1f9dcf90574e9aec1ebc8ab
Oğuzhan Çalışkan <77392426+Mhibs@users.noreply.github.com>
2022-12-18 18:06:55 +0300

    Add CV button Window
    Creating add cv button window, exit button and codes

--- CV_Maker/src/main/java/com/example/cv_maker/CV_MakerController.java
+++ CV_Maker/src/main/java/com/example/cv_maker/CV_MakerController.java
@@ -1,14 +1,53 @@
 package com.example.cv_maker;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class CV_MakerController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button createCVButton;
+    @FXML
+    private Button listButton;
+    @FXML
+    private Button loadButton;
+    @FXML
+    private Button helpButton;
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private void initialize() {
+        createCVButton.setOnAction((actionEvent -> openCreateMenu()));
+        listButton.setOnAction(actionEvent -> openList());
+        loadButton.setOnAction(actionEvent -> openLoad());
+        helpButton.setOnAction(actionEvent -> openHelp());
+        exitButton.setOnAction(actionEvent -> exitApp());
     }
+
+   @FXML
+    private void openCreateMenu(){
+
+    }
+
+    @FXML
+    private void openLoad(){
+
+    }
+    @FXML
+    private void openHelp(){
+
+    }
+    @FXML
+    private void openList(){
+
+    }
+    @FXML
+    private void exitApp(){
+        Platform.exit();
+    }
+
 }
---
+++ CV_Maker/src/main/java/com/example/cv_maker/CreateCVController.java
@@ -0,0 +1,9 @@
+package com.example.cv_maker;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CreateCVController {
+    @FXML
+    private Button saveButton;
+}
