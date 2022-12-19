654b1fdad42967751882716496f77e6cadf97c17
Oğuzhan Çalışkan <77392426+Mhibs@users.noreply.github.com>
2022-12-19 17:36:19 +0300

    GUI background and Comments
    Changing GUI background image and comment lines for teammates

--- CV_Maker/src/main/java/com/example/cv_maker/CV_Maker.java
+++ CV_Maker/src/main/java/com/example/cv_maker/CV_Maker.java
@@ -11,10 +11,13 @@ public class CV_Maker extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CV_Maker.class.getResource("CV_Maker.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 846, 460);
+        Scene scene = new Scene(fxmlLoader.load(), 688, 453);
         stage.setTitle("CV Maker");
         stage.setScene(scene);
         stage.show();
+        stage.setMinWidth(605);
+        stage.setMinHeight(405);
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {
--- CV_Maker/src/main/java/com/example/cv_maker/CV_MakerController.java
+++ CV_Maker/src/main/java/com/example/cv_maker/CV_MakerController.java
@@ -3,8 +3,17 @@ package com.example.cv_maker;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class CV_MakerController {
 
@@ -17,32 +26,52 @@ public class CV_MakerController {
     @FXML
     private Button helpButton;
     @FXML
-    private Button exitButton;
+    private Button exitButton; //buton burada fxml dosyasına bağlanıyor, scenebuilder da butonun üstüne tıklayıp sağ taraftan
+    //code kısmına geldiğinizde butonların id si ve eventlerinin bağdaştırılacağı yeri görüceksiniz, oraya yazarsanız scenebuilder
+    //otomatik olarak fxml dosyasına bağlıyor.
 
     @FXML
     private void initialize() {
-        createCVButton.setOnAction((actionEvent -> openCreateMenu()));
-        listButton.setOnAction(actionEvent -> openList());
+        createCVButton.setOnAction((actionEvent -> CreateCV())); //Butonları ilgilendiren action bu şekilde ekleniyor
+        listButton.setOnAction(actionEvent -> listCV());
         loadButton.setOnAction(actionEvent -> openLoad());
-        helpButton.setOnAction(actionEvent -> openHelp());
+        helpButton.setOnAction(actionEvent -> getHelp());
         exitButton.setOnAction(actionEvent -> exitApp());
     }
 
    @FXML
-    private void openCreateMenu(){
+   private void CreateCV() {
+       try {
+           Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("CreateCV.fxml")));
+           Stage stage = new Stage();
+           stage.initModality(Modality.APPLICATION_MODAL);
+           stage.setTitle("Create CV");
+           stage.setScene(new Scene(parent, 600, 400));
+           stage.setMinWidth(605);
+           stage.setMinHeight(405);
+           stage.setResizable(false);
 
-    }
+
+           stage.show();
+
+       } catch (IOException e) {
+           Alert alert = new Alert(Alert.AlertType.ERROR);
+           alert.setHeaderText("help fail");
+           System.out.println("**********************"+e);
+           alert.show();
+       }
+   }
 
     @FXML
     private void openLoad(){
 
     }
     @FXML
-    private void openHelp(){
+    private void getHelp(){
 
     }
     @FXML
-    private void openList(){
+    private void listCV(){
 
     }
     @FXML
