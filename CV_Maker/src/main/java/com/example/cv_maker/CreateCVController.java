46d0d77df7a9cc1fa282d504f93f9fda3ee3cb02
Oğuzhan Çalışkan <77392426+Mhibs@users.noreply.github.com>
2022-12-20 22:30:12 +0300

    .
    Git kullanımı

--- CV_Maker/src/main/java/com/example/cv_maker/CreateCVController.java
+++ CV_Maker/src/main/java/com/example/cv_maker/CreateCVController.java
@@ -2,12 +2,20 @@ package com.example.cv_maker;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class CreateCVController {
     @FXML
     private Button saveButton;
+
+    @FXML
+    private TextField nameText;
+
+    @FXML
+    private TextField idText;
     @FXML
     private void addCV(){
+
 
     }
 }
